1fe211489d5ee560e9bb2b2d8b048f97f1cdb9e1
FraSimonetti <99207304+FraSimonetti@users.noreply.github.com>
2023-11-03 12:39:01 +0100

    Update rrd.py

--- rrd.py
+++ rrd.py
@@ -2,7 +2,6 @@ import pandas as pd
 import plotly.graph_objs as go
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 
@@ -11,26 +10,6 @@ import altair as alt
 # Set the page configuration at the beginning of your script
 
 
-# Setup for Storytelling (matplotlib):
-plt.rcParams['font.family'] = 'monospace'
-plt.rcParams['font.size'] = 8
-plt.rcParams['font.weight'] = 'bold'
-plt.rcParams['figure.facecolor'] = '#464545' 
-plt.rcParams['axes.facecolor'] = '#464545' 
-plt.rcParams['axes.titleweight'] = 'bold'
-plt.rcParams['axes.titlecolor'] = 'black'
-plt.rcParams['axes.titlesize'] = 9
-plt.rcParams['axes.labelcolor'] = 'darkgray'
-plt.rcParams['axes.labelweight'] = 'bold'
-plt.rcParams['axes.edgecolor'] = 'darkgray'
-plt.rcParams['axes.linewidth'] = 0.2
-plt.rcParams['ytick.color'] = 'darkgray'
-plt.rcParams['xtick.color'] = 'darkgray'
-plt.rcParams['axes.titlecolor'] = '#FFFFFF'
-plt.rcParams['axes.titlecolor'] = 'white'
-plt.rcParams['axes.edgecolor'] = 'darkgray'
-plt.rcParams['axes.linewidth'] = 0.85
-plt.rcParams['ytick.major.size'] = 0
 
 # Page setup:
 st.set_page_config(
