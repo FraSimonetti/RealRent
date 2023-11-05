8d56080ea102cb52d46d671e506eaadd4f82ffe8
FraSimonetti <99207304+FraSimonetti@users.noreply.github.com>
2023-11-05 15:27:14 +0100

    Update rrd.py

--- rrd.py
+++ rrd.py
@@ -20,10 +20,12 @@ st.set_page_config(
 )
 st.markdown("Questa dashboard ha esclusivamente uno scopo illustrativo e dimostrativo. I dati visualizzati qui sono generici e non rappresentano alcuna situazione reale. Per ulteriori informazioni o per richiedere l'accesso alla nostra dashboard ufficiale, vi invitiamo a scriverci all'indirizzo e-mail realrent.tech@gmail.com o contattarci direttamente cliccando sul pulante qui sotto!")
 st.link_button("Contattaci", "https://real-rent.it/contact")
+st.markdown('---')
 left,center,right = st.columns([1,2, 1])
 image = Image.open('logo_white.png')
 with center:
     st.image(image)
+st.markdown('---')
 @st.cache_data
 def get_data_from_excel():
     try:
